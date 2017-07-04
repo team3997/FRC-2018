f07d4782130109ee1afbd30a7b2366b2f2875c22
Peter Chacko <workpraytoday@gmail.com>
2017-07-04 17:44:32 -0500

    added motion to autonomous

--- src/org/usfirst/frc/team3997/robot/auto/routines/RightGearRoutine.java
+++ src/org/usfirst/frc/team3997/robot/auto/routines/RightGearRoutine.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team3997.robot.auto.routines;
 import org.usfirst.frc.team3997.robot.MasterController;
 import org.usfirst.frc.team3997.robot.auto.AutoRoutine;
 
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Waypoint;
+
 public class RightGearRoutine extends AutoRoutine {
 	private MasterController controllers;
 
@@ -12,8 +15,13 @@ public class RightGearRoutine extends AutoRoutine {
 
 	@Override
 	public void prestart() {
-		// TODO Auto-generated method stub
-		
+		//TODO add real values
+		controllers.getMotionController().setUp(new Waypoint[] {
+			    new Waypoint(-4, -1, Pathfinder.d2r(-70)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+			    new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
+			    new Waypoint(0, 0, 0)                           // Waypoint @ x=0, y=0,   exit angle=0 radians
+			});
+		controllers.getRobotModel().resetGyro();
 	}
 
 	@Override
@@ -27,6 +35,7 @@ public class RightGearRoutine extends AutoRoutine {
 		driveDistanceStraight(controllers, -15.0, .95, 2, true);
 		driveRotate(controllers, 70, 0.6, 1.6, true);
 		arcadeDistanceStraight(controllers, 75.0, .95, 4.0, 0.4);
+		//TODO controllers.getMotionController().enable();
 	}
 
 }
--- src/org/usfirst/frc/team3997/robot/controllers/MotionController.java
+++ src/org/usfirst/frc/team3997/robot/controllers/MotionController.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team3997.robot.controllers;
 
+import org.usfirst.frc.team3997.robot.Params;
 import org.usfirst.frc.team3997.robot.hardware.RobotModel;
 
 import jaci.pathfinder.*;
@@ -8,7 +9,7 @@ import jaci.pathfinder.modifiers.TankModifier;
 
 public class MotionController {
 	private RobotModel robot;
-	public Waypoint[] points;
+	private Waypoint[] points;
 	public Trajectory.Config config;
 	public Trajectory trajectory;
 	public TankModifier modifier;
@@ -17,51 +18,52 @@ public class MotionController {
 
 	public MotionController(RobotModel robot) {
 		this.robot = robot;
-		points = new Waypoint[] { new Waypoint(-4, -2, Pathfinder.d2r(-45)), new Waypoint(-2, -2, 0),
-				new Waypoint(0, 0, 0) };
 
-		config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7,
-				2.0, 60.0);
+	}
+
+	public void setUp(Waypoint[] points) {
+		this.points = points;
+		config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, Params.maximum_velocity,
+				Params.maximum_acceleration, Params.maximum_jerk);
 		trajectory = Pathfinder.generate(points, config);
 
 		// TODO find distance between front and rear axles of a vehicle
 		modifier = new TankModifier(trajectory).modify(0.5);
 		left = new EncoderFollower(modifier.getLeftTrajectory());
 		right = new EncoderFollower(modifier.getRightTrajectory());
-
 	}
-	//TODO Put this in control loop
+
+	// TODO Put this in control loop
 	public void enable() {
-		//TODO get max velocity
-		//TODO find ticks_per_revolution 
-				//.1016 meters = 4 inch wheel diameter
+		// TODO get max velocity
+		// TODO find ticks_per_revolution
+		// .1016 meters = 4 inch wheel diameter
 		robot.resetGyro();
 		left.configureEncoder(robot.leftDriveEncoder.get(), 100, .1016);
 		right.configureEncoder(robot.rightDriveEncoder.get(), 100, .1016);
-		left.configurePIDVA(1.0, 0.0, 0.0, (1/1.7), 0);
-		right.configurePIDVA(1.0, 0.0, 0.0, (1/1.7), 0);
-		
+		left.configurePIDVA(1.0, 0.0, 0.0, (1 / Params.maximum_velocity), 0);
+		right.configurePIDVA(1.0, 0.0, 0.0, (1 / Params.maximum_velocity), 0);
+
 		double l = left.calculate(robot.leftDriveEncoder.get());
 		right.configureEncoder(robot.leftDriveEncoder.get(), 100, .1016);
 		right.configureEncoder(robot.rightDriveEncoder.get(), 100, .1016);
-		right.configurePIDVA(1.0, 0.0, 0.0, (1/1.7), 0);
-		right.configurePIDVA(1.0, 0.0, 0.0, (1/1.7), 0);
-		
+		right.configurePIDVA(1.0, 0.0, 0.0, (1 / Params.maximum_velocity), 0);
+		right.configurePIDVA(1.0, 0.0, 0.0, (1 / Params.maximum_velocity), 0);
+
 		double r = left.calculate(robot.rightDriveEncoder.get());
 		double gyro_heading = robot.getAngle();
 		double desired_heading = Pathfinder.r2d(left.getHeading());
-		
+
 		double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - gyro_heading);
-		double turn = 0.8 * (-1.0/80) * angleDifference;
-		
+		double turn = 0.8 * (-1.0 / 80) * angleDifference;
+
 		robot.setLeftMotors(l + turn);
 		robot.setRightMotors(r - turn);
 	}
-	
 
 	public void disable() {
 		robot.setLeftMotors(0);
 		robot.setRightMotors(0);
 	}
-	
+
 }
