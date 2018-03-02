d33d8829414b0da37a27af086be496ea82d8df77
Trial-N-Err <>
2018-03-01 21:02:02 -0600

    Cleaned up ArmController kTeleop - not done yet

--- src/org/usfirst/frc/team3997/robot/controllers/ArmController.java
+++ src/org/usfirst/frc/team3997/robot/controllers/ArmController.java
@@ -24,8 +24,13 @@ public class ArmController {
 
 	private ArmState m_stateVal;
 	private ArmState nextState;
-	private boolean toggleArmManual;
-	private boolean toggleCollapse;
+	
+	// during 2017 competition the gear intake mech lost control and made the motors smoke
+	// We needed a way to toggle between automatic mode and manual - this is what we are going for 
+	// with toggleArmManual - no smoking!
+	private boolean toggleArmManual;	// toggles the arm subsystem manual vs automatic using PID
+	
+	private boolean toggleCollapse;	  	// opens and closes the intake false is what ever the current position is
 
 	public enum ArmState {
 		kInitialize, kTeleop
@@ -45,67 +50,70 @@ public class ArmController {
 		armPIDController.setAbsoluteTolerance(0.25);
 		armPIDController.setSetpoint(Params.ARM_REST_SETPOINT);
 		armPIDController.disable();
-		
-		
+				
 		m_stateVal = ArmState.kInitialize;
-		nextState = ArmState.kInitialize;
 	}
 
 	public void reset() {
 		m_stateVal = ArmState.kInitialize;
+		//?? should we also be disabling the arm?
 	}
 
 	// ???
 	public void update() {
 		switch(m_stateVal) {
-		case kInitialize:
-			
-			
-			armPIDController.setPID(Params.arm_p,Params.arm_i,Params.arm_d,Params.arm_f);
-			armPIDController.setOutputRange(-1, 1);
-			armPIDController.setSetpoint(Params.ARM_REST_SETPOINT);
-			toggleArmManual = false;
-			toggleCollapse = false;
-			nextState = ArmState.kTeleop;
-			break;
-		case kTeleop:
-			if(humanControl.toggleManualArmDesired())
-				toggleArmManual = !toggleArmManual;
-			if(humanControl.toggleCollapseIntake()) {
-				toggleCollapse = !toggleCollapse;
-			}
-			//Arm Behavior
-			if (toggleArmManual) {
-				robot.moveArm(humanControl.getJoystickValue(Joysticks.kOperatorJoy, RemoteControl.Axes.kRY));
+			case kInitialize:
+				armPIDController.setPID(Params.arm_p,Params.arm_i,Params.arm_d,Params.arm_f);
+				armPIDController.setOutputRange(-1, 1);
+				armPIDController.setAbsoluteTolerance(0.25);
+				armPIDController.setSetpoint(Params.ARM_REST_SETPOINT);
+				toggleArmManual = false; //
+				toggleCollapse = false;  //
+				m_stateVal = ArmState.kTeleop;
+				break;
+			case kTeleop:
+				// check if we need to change into manual mode
+				if(humanControl.toggleManualArmDesired())
+					toggleArmManual = !toggleArmManual;
+				
+				// check if we need to change the intake collapse state
+				if(humanControl.toggleCollapseIntake()) {
+					toggleCollapse = !toggleCollapse;
+				}
+				
+				// If we are in manual mode - read the arm values from the joystick
+				// otherwise we will be reading values from the PID controller - which needs to be disabled first
+				// to get a setpoint.
+				if (toggleArmManual) {
+					robot.moveArm(humanControl.getJoystickValue(Joysticks.kOperatorJoy, RemoteControl.Axes.kRY));
+				} else {
+					if (armPIDController.isEnabled()) {
+						armPIDController.disable();
+					}
+				}
+				
+				// Decide what intake movement is needed (if any)
+				// Should only be doing one - but in the case that the operator hits both buttons
+				// perform the intake function
+				// If neither button is pressed - stop the motors
 				if(humanControl.getIntakeDesired()) {
 					intakeBlock();
-				} else if(humanControl.getOuttakeDesired()) {
-					robot.openIntake();
+				}else if(humanControl.getOuttakeDesired()) {
 					outtakeBlock();
 				}
+				else {
+					stopIntake();
+				}
+				
+				// Decide if intake should open or close (if any)
+				// Should only be one - in the case that both buttons are pressed, close intake
 				if(toggleCollapse) {
 					robot.closeIntake();
 				} else {
 					robot.openIntake();
 				}
-				
-			} else {
-				if (armPIDController.isEnabled()) {
-					armPIDController.disable();
-				}
-				
-				if(humanControl.getIntakeDesired()) {
-					intakeBlock();
-				} else if(humanControl.getOuttakeDesired()) {
-					robot.openIntake();
-					outtakeBlock();
-				}
-				if(toggleCollapse) {
-					robot.closeIntake();
-				} else {
-					robot.openIntake();
-				}
-				
+					
+				// Other arm movements and PID stuff	
 				if(humanControl.getClimbArmDesired()) {
 					goToClimbPosition();
 				} else if(humanControl.getScaleArmDesired()) {
@@ -115,15 +123,10 @@ public class ArmController {
 				} else if(humanControl.getFeedArmDesired()) {
 					goToFeedPosition();
 				}
-			}
-			
-			/* The else {} section above will just disable the PID controller every single loop if not in manual control.
-			Have you just not added in logic for the functions below yet?  -Aaron  */
-			
-			nextState = ArmState.kTeleop;
-			break;
+
+				break;
 		}
-		m_stateVal = nextState;
+		
 	}
 	
 	public void goToSwitchPosition() {
@@ -153,14 +156,17 @@ public class ArmController {
 		armPIDController.setSetpoint(Params.ARM_FEED_SETPOINT);
 		armPIDController.enable();
 	}
-	
+
 	public void intakeBlock() {
-		robot.intakeBlock(.7);
+		robot.intakeBlock(0.7);
 	}
 	
-
 	public void outtakeBlock() {
 		robot.outtakeBlock(0.3);
+	}
+	
+	public void stopIntake() {
+		robot.intakeBlock(0);
 	}
 	
 	public void stop() {
